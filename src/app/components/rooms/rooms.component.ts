61f876269f8e5f5659ecfe08b574f88c230cc821
wellingtonlopes <wellingtonpblopes@gmail.com>
2023-02-08 09:00:14 -0300

    Add filter logic for filtering room list by room type

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -3,7 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
 import { first } from 'rxjs';
 
-import { RoomInterface } from 'src/app/interfaces/room.interface';
+import { RoomInterface, RoomTypeEnum } from 'src/app/interfaces/room.interface';
 import { RoomsService } from 'src/app/services/rooms.service';
 import { RoomDetailsDialogComponent } from './room-details-dialog/room-details-dialog.component';
 
@@ -15,6 +15,12 @@ import { RoomDetailsDialogComponent } from './room-details-dialog/room-details-d
 export class RoomsComponent implements OnInit {
   public roomList!: RoomInterface[];
   public totalRooms!: number;
+  public currentTotalRooms!: number;
+  public singleRoomsTotal!: number;
+  public doubleRoomsTotal!: number;
+  public deluxeRoomsTotal!: number;
+  public suitesTotal!: number;
+  public presidentialSuitesTotal!: number;
 
   private readonly FIRST_PAGE = 0;
 
@@ -22,6 +28,11 @@ export class RoomsComponent implements OnInit {
 
   public ngOnInit(): void {
     this.getRoomsByPage(this.FIRST_PAGE);
+    this.roomsService.getRoomQuantity(RoomTypeEnum.SINGLE_ROOM).pipe(first()).subscribe(response => this.singleRoomsTotal = response);
+    this.roomsService.getRoomQuantity(RoomTypeEnum.DOUBLE_ROOM).pipe(first()).subscribe(response => this.doubleRoomsTotal = response);
+    this.roomsService.getRoomQuantity(RoomTypeEnum.DELUXE_ROOM).pipe(first()).subscribe(response => this.deluxeRoomsTotal = response);
+    this.roomsService.getRoomQuantity(RoomTypeEnum.SUITE).pipe(first()).subscribe(response => this.suitesTotal = response);
+    this.roomsService.getRoomQuantity(RoomTypeEnum.PRESIDENTIAL_SUITE).pipe(first()).subscribe(response => this.presidentialSuitesTotal = response);
   }
 
   public openDetailsDialog(room: RoomInterface): void {
@@ -39,11 +50,18 @@ export class RoomsComponent implements OnInit {
     this.getRoomsByPage(event.pageIndex);
   }
 
+  public filterList(filterBy: string) {
+    this.roomsService.getFilteredRooms(filterBy).pipe(first()).subscribe(response => {
+      this.roomList = response.roomList;
+      this.currentTotalRooms = response.totalCount;
+    })
+  }
 
   private getRoomsByPage(pageIndex: number) {
     this.roomsService.getRooms(pageIndex).pipe(first()).subscribe(response => {
       this.roomList = response.roomList;
       this.totalRooms = response.totalCount;
+      this.currentTotalRooms = this.totalRooms;
     });
   }
 }
--- src/app/services/rooms.service.ts
+++ src/app/services/rooms.service.ts
@@ -1,43 +1,71 @@
 import { Injectable } from '@angular/core';
 import { from, Observable, reduce, throwError } from 'rxjs';
 import { MOCKED_ROOMS } from '../components/shared/mock-database';
-import { RoomInterface, RoomResponseInterface } from '../interfaces/room.interface';
+import { RoomInterface, RoomResponseInterface, RoomTypeEnum } from '../interfaces/room.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class RoomsService {
-  constructor() { }
+  private roomInterfaceList: RoomInterface[][];
+  private totalItemCount!: number;
 
-  getRooms(pageIndex: number): Observable<RoomResponseInterface> {
-    const roomInterfaceList = JSON.parse(MOCKED_ROOMS) as RoomInterface[][];
-    const totalItemCount = roomInterfaceList.reduce((acc, currentPageArray) => acc + currentPageArray.length, 0);
+  constructor() {
+    this.roomInterfaceList = JSON.parse(MOCKED_ROOMS) as RoomInterface[][];
+    this.totalItemCount = this.roomInterfaceList.reduce((acc, currentPageArray) => acc + currentPageArray.length, 0);
+  }
 
+  public getRooms(pageIndex: number): Observable<RoomResponseInterface> {
     switch (pageIndex) {
       case 0:
         const pageResponse1: RoomResponseInterface[] = [{
           roomList: JSON.parse(MOCKED_ROOMS)[0],
-          totalCount: totalItemCount
+          totalCount: this.totalItemCount
         }];
         return from(pageResponse1);
 
       case 1:
         const pageResponse2: RoomResponseInterface[] = [{
           roomList: JSON.parse(MOCKED_ROOMS)[1],
-          totalCount: totalItemCount
+          totalCount: this.totalItemCount
         }];
         return from(pageResponse2);
 
       case 2:
         const pageResponse3: RoomResponseInterface[] = [{
           roomList: JSON.parse(MOCKED_ROOMS)[2],
-          totalCount: totalItemCount
+          totalCount: this.totalItemCount
         }];
         return from(pageResponse3);
 
       default:
         throw new Error("Page index out of bounds");
     }
+  }
+
+  getFilteredRooms(filter: string): Observable<RoomResponseInterface> {
+    if (filter === 'ALL') {
+      return this.getRooms(0);
+    }
+
+    const filteredRooms = this.roomInterfaceList.map(page => {
+      return page.filter(room => room.roomType === filter);
+    }).flat(2);
+
+    return from([{
+      roomList: filteredRooms,
+      totalCount: filteredRooms.length
+    }]);
+  }
+
+  public getRoomQuantity(roomType: RoomTypeEnum): Observable<number> {
+    const numberOfRooms = this.roomInterfaceList
+      .map(page => {
+        return page.filter(room => room.roomType === roomType);
+      })
+      .reduce((acc, currentPageArray) => acc + currentPageArray.length, 0);
+
+    return from([numberOfRooms]);
   }
 }
