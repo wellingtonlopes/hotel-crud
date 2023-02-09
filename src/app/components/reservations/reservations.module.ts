2da33d2a2ea7c2db18c4a3d95f2b89aa19e64b9e
wellingtonlopes <wellingtonpblopes@gmail.com>
2023-02-09 09:47:51 -0300

    create reservations read component and module

---
+++ src/app/components/reservations/reservations.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservationsComponent } from './reservations.component';
+
+
+
+@NgModule({
+  declarations: [
+    ReservationsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReservationsModule { }
