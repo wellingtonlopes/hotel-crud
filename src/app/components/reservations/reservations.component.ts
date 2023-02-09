fd7d1ef6486fa4a8a7f58bad384b8eb5837c0862
wellingtonlopes <wellingtonpblopes@gmail.com>
2023-02-09 17:11:30 -0300

    Implement list view for the reservations entity

--- src/app/components/reservations/reservations.component.ts
+++ src/app/components/reservations/reservations.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { first } from 'rxjs';
+
+import { ReservationInterface } from 'src/app/interfaces/reservation.interface';
+import { ReservationsService } from 'src/app/services/reservations.service';
+import { Constants } from 'src/app/shared/contants';
+import { ReservationsSource } from './reservations-data-source';
 
 @Component({
   selector: 'app-reservations',
@@ -6,10 +13,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservations.component.scss']
 })
 export class ReservationsComponent implements OnInit {
+  public reservationsDataSource = new ReservationsSource();
+  public totalCount!: number;
+  public readonly pageSize = 5;
+  public readonly hasAddFunctionality = true;
+  public readonly displayedColumns: string[] = ['id', 'reserved-by', 'reserved-at', 'actions'];
 
-  constructor() { }
+  private reservationList!: ReservationInterface[];
+
+  constructor(private readonly reservationsService: ReservationsService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getPaginatedReservations(Constants.FIRST_PAGE);
   }
 
+  public changePage(event: number): void {
+    this.getPaginatedReservations(event);
+  }
+
+  public goToElementDetails(reservationId: number): void {
+    this.router.navigate([`${Constants.PATH.RESERVATIONS}/${reservationId}`]);
+  }
+
+  public deleteElement(reservationId: number): void {
+    //DELETE OBJECT --- TO BE IMPLEMENTED ---
+  }
+
+  private getPaginatedReservations(pageIndex: number) {
+    this.reservationsService.getReservations(pageIndex).pipe(first()).subscribe(response => {
+      this.reservationList = response.reservationList;
+      this.totalCount = response.totalCount;
+      this.reservationsDataSource.loadReservations(this.reservationList);
+    });
+  }
 }
--- src/app/components/reservations/reservations.module.ts
+++ src/app/components/reservations/reservations.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+
 import { ReservationsComponent } from './reservations.component';
+import { SharedComponentsModule } from '../shared-components/shared-components.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -9,7 +14,12 @@ import { ReservationsComponent } from './reservations.component';
     ReservationsComponent
   ],
   imports: [
+    CommonModule,
+    SharedComponentsModule,
+    MatTableModule,
     CommonModule
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class ReservationsModule { }
